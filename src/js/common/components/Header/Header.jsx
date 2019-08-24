f8d89c39f3cb5f4080f398afe2f0481106c280d3
PigletWebDev <piglet.web.dev@outlook.com>
2019-08-23 17:35:34 -0700

    fix header connectionstate add hamburgermenu

--- src/js/common/components/Header/Header.jsx
+++ src/js/common/components/Header/Header.jsx
@@ -2,7 +2,7 @@ import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { Spinner } from 'react-bootstrap';
-
+import HamburgerMenu from 'react-hamburger-menu'
 import './Header.scss';
 
 import imgLogo from '../../../../assets/images/logo.png';
@@ -91,7 +91,6 @@ class Header extends PureComponent {
     if(initializing) {
       return (
       <div className='div-disconnected'>
-        <span></span>
         <p>
           Connecting MetaMask...
         </p>
@@ -118,41 +117,73 @@ class Header extends PureComponent {
           </React.Fragment>)
       } else {
         return (<div className='div-disconnected'>
-          <div className='circle orange'/>
-            <span></span>
-            <p className='nohover'>Connect to Ethereum Main Net</p>          
+          <div className='circle red'/>
+            <p className='nohover'>Connect to Main Net</p>          
         </div>)
       }
     } else {
       if(!error) {
         return (
           <div className='div-disconnected'>
-            <span></span>
+            <div className='circle orange'/>
             <p onClick={this.initCreate}>Connect to MetaMask</p>
           </div>
         )
       } else if( networkId != 1 ) {
         return (<div className='div-disconnected'>
-          <div className='circle orange'/>
-          <span></span>
-          <p onClick={this.initCreate}>Connect to Ethereum Main Net</p>          
+          <div className='circle red'/>
+          <p onClick={this.initCreate}>Connect to Main Net</p>          
         </div>)
 
       } else if (error === 'user denied site access') {
         return (<div className='div-disconnected'>
-          <div className='circle orange'/>
-          <span></span>
-          <p>Connect to Ethereum Main Net</p>          
+          <div className='circle red'/>
+          <p>Connect to Main Net</p>          
         </div>)       
       } else if (error === 'non-ethereum browser detected') {
         return (
           <div className='div-disconnected'>
-            <span></span>
+            <div className='circle orange'/>
             <p onClick={this.initCreate}>Connect to MetaMask</p>
           </div>
         )
       }
     }
+  }
+  hamburgerMenu = () => {
+    const { menuOpen } = this.state
+    return (
+      <div className="hamburger-menu">
+        <HamburgerMenu
+          isOpen={menuOpen}
+          menuClicked={this.handleMenuClick}
+          width={18}
+          height={12}
+          strokeWidth={3}
+          rotate={0}
+          color="#4a4a4a"
+          borderRadius={0}
+          animationDuration={0.5}
+        />
+        {menuOpen && (
+          <div
+            className={menuOpen ? 'dropdown-content show' : 'dropdown-content'}
+          >
+            <ul>
+              <li><a>Create</a></li>
+              <li><a>Explore</a></li>
+              <li><a>Search</a></li>
+              <li><a>Govern</a></li>
+              <li><a>Account</a></li>
+            </ul>
+          </div>
+        )}
+      </div>
+    )
+  }
+
+  handleMenuClick = () => {
+    this.setState({ menuOpen: !this.state.menuOpen })
   }
   render() {
     // const { location } = this.props;
@@ -166,12 +197,13 @@ class Header extends PureComponent {
         <div className='row'>
           <div className='div-logo '>
             <img className='img-logo disabled' src={imgLogo}/> 
-            {/* <span className='span-title'>Explore</span> */}
+            <span className='span-title'>Explore</span>
             <DropDownCaptionMenu />
           </div>
           <div className='div-bal '>
             {this.ConnectionState()}
           </div>
+          {this.hamburgerMenu()}
         </div>
       </header>
     );
